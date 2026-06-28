4195bc8ce29952784b6b27f161c18264bdb17ca1
tjswlsdl <166943752+tjswlsdl@users.noreply.github.com>
2026-06-28 15:16:56 +0900

    [Passed] 변수 값 복사하기 3 (1ms, 0MB)

---
+++ trail0/변수 값 복사하기 3/copying-variable-values-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a=1, b=5, c=3;
+    a=c;
+    a=a+c;
+    b=b-c;
+    printf("%d\n%d\n%d",a,b,c);
+
+    return 0;
+}
